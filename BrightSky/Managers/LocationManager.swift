7bbeb3ad5d249a81748d0ffd477e801b3abd4f8b
Shaw Lu <shawlu95@gmail.com>
2023-12-10 19:58:42 -0800

    fetch location and execute callback on setter

--- BrightSky/Managers/LocationManager.swift
+++ BrightSky/Managers/LocationManager.swift
@@ -10,9 +10,22 @@ import Foundation
 
 final class LocationManager: NSObject, CLLocationManagerDelegate {
     private let manager = CLLocationManager()
+    private var locationFetchCompletion: ((CLLocation) -> Void)?
+    private var location: CLLocation? {
+        didSet {
+            // call the callback when location is set
+            guard let location = location else {
+                return
+            }
+            locationFetchCompletion?(location)
+        }
+    }
+    
     static let shared = LocationManager()
     
-    public func getCurrentLocation() {
+    public func getCurrentLocation(completion: @escaping (CLLocation) -> Void) {
+        // pass in a callback, to be executed when setting location
+        self.locationFetchCompletion = completion
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.startUpdatingLocation()
@@ -23,6 +36,7 @@ final class LocationManager: NSObject, CLLocationManagerDelegate {
         guard let location = locations.first else {
             return
         }
+        self.location = location
         // save user battery life
         manager.stopUpdatingLocation()
     }
