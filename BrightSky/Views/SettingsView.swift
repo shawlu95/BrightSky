a200d3332a49ad0df3f16979f5b175aa406487ce
Shaw Lu <shawlu95@gmail.com>
2023-12-11 00:23:44 -0800

    set up table view skeleton

--- BrightSky/Views/SettingsView.swift
+++ BrightSky/Views/SettingsView.swift
@@ -7,16 +7,51 @@
 
 import UIKit
 
-class SettingsView: UIView {
+class SettingsView: UIView, UITableViewDataSource, UITableViewDelegate {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .red
         // we manually set constraint
         translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(
+                equalTo: bottomAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK : - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: indexPath
+        )
+        cell.textLabel?.text = "Hello World"
+        return cell
     }
 }
