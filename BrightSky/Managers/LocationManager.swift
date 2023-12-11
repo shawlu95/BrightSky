9fc0f794ab8cee17da3e35199185848c9b37ead2
Shaw Lu <shawlu95@gmail.com>
2023-12-10 19:55:05 -0800

    request location

--- BrightSky/Managers/LocationManager.swift
+++ BrightSky/Managers/LocationManager.swift
@@ -8,6 +8,22 @@
 import CoreLocation
 import Foundation
 
-final class LocationManager: NSObject {
+final class LocationManager: NSObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
     static let shared = LocationManager()
+    
+    public func getCurrentLocation() {
+        manager.requestWhenInUseAuthorization()
+        manager.delegate = self
+        manager.startUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        // save user battery life
+        manager.stopUpdatingLocation()
+    }
 }
