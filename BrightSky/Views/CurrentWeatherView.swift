7edb461c5ab0d057d1419111afb8fca4e0315246
Shaw Lu <shawlu95@gmail.com>
2023-12-12 01:16:20 -0800

    display one hourly cell taking 1/4 of screen width

--- BrightSky/Views/CurrentWeatherView.swift
+++ BrightSky/Views/CurrentWeatherView.swift
@@ -67,17 +67,21 @@ class CurrentWeatherView: UIView {
             let item = NSCollectionLayoutItem(layoutSize: .init(
                 widthDimension: .fractionalWidth(1.0),
                 heightDimension: .fractionalHeight(1.0)))
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .fractionalHeight(0.75)), subitems: [item])
-            return NSCollectionLayoutSection(group: group)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(
+                widthDimension: .fractionalWidth(0.25),
+                heightDimension: .absolute(150)), subitems: [item])
+            group.contentInsets = .init(top: 1, leading: 2, bottom: 1, trailing: 2)
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            return section
         case .daily:
             let item = NSCollectionLayoutItem(layoutSize: .init(
                 widthDimension: .fractionalWidth(1.0),
                 heightDimension: .fractionalHeight(1.0)))
             let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(
                 widthDimension: .fractionalWidth(1.0),
-                heightDimension: .fractionalHeight(0.75)), subitems: [item])
+                heightDimension: .absolute(100)), subitems: [item])
+            group.contentInsets = .init(top: 2, leading: 2, bottom: 2, trailing: 2)
             return NSCollectionLayoutSection(group: group)
         }
     }
@@ -89,7 +93,10 @@ extension CurrentWeatherView: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        if section == 0 {
+            return 1
+        }
+        return 12
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
