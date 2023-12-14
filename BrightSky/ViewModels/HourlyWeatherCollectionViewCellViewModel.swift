ce4565fb4cec127d669c2de4707f284452dc11a2
Shaw Lu <shawlu95@gmail.com>
2023-12-13 22:04:29 -0800

    display info in hourly weather cell
    Update DailyWeatherCollectionViewCell.swift

--- BrightSky/ViewModels/HourlyWeatherCollectionViewCellViewModel.swift
+++ BrightSky/ViewModels/HourlyWeatherCollectionViewCellViewModel.swift
@@ -13,4 +13,17 @@ struct HourlyWeatherCollectionViewCellViewModel {
     init(model: HourWeather) {
         self.model = model
     }
+    
+    public var iconName: String {
+        return model.symbolName
+    }
+    
+    public var temperature: String {
+        return "\(Int(model.temperature.converted(to: .fahrenheit).value)) °F"
+    }
+    
+    public var hour: String {
+        let hour = Calendar.current.component(.hour, from: model.date)
+        return "\(hour):00"
+    }
 }
--- BrightSky/Views/WeatherCell/DailyWeatherCollectionViewCell.swift
+++ BrightSky/Views/WeatherCell/DailyWeatherCollectionViewCell.swift
@@ -23,4 +23,28 @@ class DailyWeatherCollectionViewCell: UICollectionViewCell {
     public func configure(with viewModel: DailyWeatherCollectionViewCellViewModel) {
         
     }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            timeLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            timeLabel.heightAnchor.constraint(equalToConstant: 40),
+
+            icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            icon.leftAnchor.constraint(equalTo: timeLabel.rightAnchor, constant: 10),
+            icon.heightAnchor.constraint(equalToConstant: 32),
+
+            tempLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            tempLabel.leftAnchor.constraint(equalTo: icon.rightAnchor, constant: 20),
+            tempLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -15),
+        ])
+    }
+    
+    override func prepareForReuse() {
+         super.prepareForReuse()
+
+         tempLabel.text = nil
+         timeLabel.text = nil
+         icon.image = nil
+     }
 }
